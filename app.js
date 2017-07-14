e6756f88b5a7e576beb238da5be38b468d46e6c6
Owen Hines <owenhines07@gmail.com>
2017-07-14 11:49:35 -0700

    Static folder, routes file, views folder w/ layouts and landing page.

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var mongoose = require('mongoose');
 var express = require('express');
 var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
+var routes = require('./routes/routes');
+
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -12,6 +14,8 @@ app.use(bodyParser.urlencoded({
   extended: false
 }));
 
+app.use(express.static(path.join(__dirname, 'static')));
+
 app.use(bodyParser.json());
 
 app.set('views', path.join(__dirname, 'views'));
@@ -19,5 +23,7 @@ app.engine('.hbs', exphbs({
   defaultLayout: 'main',
   extname: '.hbs'
 }));
+
+app.use('/', routes);
 
 app.listen(process.env.PORT || 3000);
