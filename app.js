938d783b73c5264dc84fa433c70e9fa8e1309613
Jack <jdrosenblatt18@gmail.com>
2017-07-14 13:49:18 -0700

    fixed login authentication

--- app.js
+++ app.js
@@ -3,24 +3,21 @@ var mongoose = require('mongoose');
 var express = require('express');
 var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+
 var User = require('./models/models').User;
 
 var auth = require('./routes/auth');
 var routes = require('./routes/routes');
 
-mongoose.connect(process.env.MONGODB_URI);
-
 var app = express();
 
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-
-app.use(express.static(path.join(__dirname, 'static')));
-
-app.use(bodyParser.json());
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI);
 
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'views'));
@@ -28,6 +25,21 @@ app.engine('.hbs', exphbs({
   defaultLayout: 'main',
   extname: '.hbs'
 }));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(cookieParser())
+
+app.use(express.static(path.join(__dirname, 'static')));
+
+app.use(session({
+  secret: process.env.SECRET,
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection
+  })
+}))
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -61,7 +73,7 @@ passport.use(new LocalStrategy(function(username, password, done) {
     });
 }));
 
-// app.use('/', auth(passport));
+app.use('/', auth(passport));
 app.use('/', routes);
 
 app.listen(process.env.PORT || 3000);
