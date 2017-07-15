a08f4319fb8e339ef9c94d68a488b68c9d7d268f
Jack <jdrosenblatt18@gmail.com>
2017-07-14 21:39:11 -0700

    removed excess commenting

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var mongoose = require('mongoose');
-var deepPopulate = require('mongoose-deep-populate')(mongoose);
 var router = express.Router();
 var User = require('../models/models').User;
 var Skill = require('../models/models').Skill;
@@ -48,83 +47,16 @@ router.post('/getSkills', function(req, res) {
     })
     .then(function(skillsArr) {
 
-      // var returnObj = {};
-      //
       skillsArr = skillsArr.sort(function(a, b) {
         return a.level > b.level;
       })
 
       console.log(skillsArr[0]);
       res.json(skillsArr[0]);
-      //
-      // Skill.find({
-      //     level: 1,
-      //     owner: req.user._id
-      //   })
-      //   .then(function(skillsArr2) {
-      //     skillsArr2.forEach(function(skill) {
-      //       returnObj.children.push({
-      //         name: skill.name,
-      //         parent: 'Skills'
-      //       });
-      //     })
-      //
-      //     var maxLevel = skillsArr[skillsArr.length - 1].level;
-      //
-      //     // console.log(skillsArr2);
-      //
-      //     // console.log('max level', maxLevel)
-      //     // for (var i = 1; i < maxLevel; i++) {
-      //     //   Skill.find({
-      //     //       level: i,
-      //     //       owner: req.user._id
-      //     //     })
-      //     //     .then(function(skillsArr3) {
-      //     //       i--;
-      //     //       console.log('skills arr3', skillsArr3)
-      //     //       skillsArr3.forEach(function(skill, index) {
-      //     //         if (skill.children.length > 0) {
-      //     //           Skill.find({
-      //     //               level: (i + 1),
-      //     //               owner: req.user._id,
-      //     //               parent: skill._id
-      //     //             })
-      //     //             .then(function(skillsArr4) {
-      //     //               console.log('skills arr4', skillsArr4)
-      //     //               skillsArr4.forEach(function(skill2, index2) {
-      //     //                 var tempObj = {
-      //     //                   name: skill2.name,
-      //     //                   parent: skill.name,
-      //     //                   children: []
-      //     //                 }
-      //     //                 console.log('tempObj', tempObj);
-      //     //                 //   returnObj.children[index]
-      //     //               })
-      //     //             })
-      //     //         }
-      //     //       })
-      //     //     })
-      //     // }
     })
-
-
-    // skillsArr.forEach(function(skill) {
-    //   var parent;
-    //   if (skill.parent) {
-    //     parent = skill.parent.name;
-    //   } else {
-    //     parent = 'Skills';
-    //   }
-    // returnObj.children.push({
-    //   name: skill.name,
-    //   parent: parent
-    // });
-    // });
-    // res.json(returnObj);
     .catch(function(err) {
       console.log(err);
     });
 })
-// });
 
 module.exports = router;
