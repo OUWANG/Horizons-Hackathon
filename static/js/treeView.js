6b1fcde7291641a20e54703ca6770cd2f94d81be
Jack <jdrosenblatt18@gmail.com>
2017-07-15 05:49:29 -0700

    fixed stuff

--- static/js/treeView.js
+++ static/js/treeView.js
@@ -1,126 +1,252 @@
-var treeData = [];
+  var treeData = [];
 
-$.ajax({
-  url: '/getSkills',
-  method: 'post',
-  success: function(resp) {
-    treeData.push(resp);
-    var margin = {
-        top: 40,
-        right: 120,
-        bottom: 20,
-        left: 120
-      },
-      width = 960 - margin.right - margin.left,
-      height = 500 - margin.top - margin.bottom;
+  $.ajax({
+    url: '/getSkills',
+    method: 'post',
+    success: function(resp) {
+      treeData.push(resp);
+      var margin = {
+          top: 40,
+          right: 120,
+          bottom: 20,
+          left: 120
+        },
+        width = 960 - margin.right - margin.left,
+        height = 500 - margin.top - margin.bottom;
 
-    var i = 0;
+      var i = 0;
 
-    var tree = d3.layout.tree()
-      .size([height, width]);
+      var tree = d3.layout.tree()
+        .size([height, width]);
 
-    var diagonal = d3.svg.diagonal()
-      .projection(function(d) {
-        return [d.x, d.y];
-      });
-
-    var svg = d3.select(".mainTree").append("svg")
-      .attr("width", width + margin.right + margin.left)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    root = treeData[0];
-
-    update(root);
-
-    function update(source) {
-
-      // Compute the new tree layout.
-      var nodes = tree.nodes(root).reverse(),
-        links = tree.links(nodes);
-
-      // Normalize for fixed-depth.
-      nodes.forEach(function(d) {
-        d.y = d.depth * 100;
-      });
-
-      // Declare the nodes…
-      var node = svg.selectAll("g.node")
-        .data(nodes, function(d) {
-          return d.id || (d.id = ++i);
+      var diagonal = d3.svg.diagonal()
+        .projection(function(d) {
+          return [d.x, d.y];
         });
 
-      // Enter the nodes.
-      var nodeEnter = node.enter().append("g")
-        .attr("class", "node")
-        .attr("transform", function(d) {
-          return "translate(" + d.x + "," + d.y + ")";
+      var svg = d3.select(".mainTree").append("svg")
+        .attr("width", width + margin.right + margin.left)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      root = treeData[0];
+
+      update(root);
+
+      function update(source) {
+
+        // Compute the new tree layout.
+        var nodes = tree.nodes(root).reverse(),
+          links = tree.links(nodes);
+
+        // Normalize for fixed-depth.
+        nodes.forEach(function(d) {
+          d.y = d.depth * 100;
         });
 
-      nodeEnter.append("circle")
-        .attr("r", 10)
-        .style("fill", "#fff");
+        // Declare the nodes…
+        var node = svg.selectAll("g.node")
+          .data(nodes, function(d) {
+            return d.id || (d.id = ++i);
+          });
 
-      nodeEnter.append("text")
-        .attr("y", function(d) {
-          return d.children || d._children ? -18 : 18;
-        })
-        .attr("dy", ".35em")
-        .attr("text-anchor", "middle")
-        .text(function(d) {
-          return d.name;
-        })
-        .style("fill-opacity", 1);
+        // Enter the nodes.
+        var nodeEnter = node.enter().append("g")
+          .attr("class", "node")
+          .attr("transform", function(d) {
+            return "translate(" + d.x + "," + d.y + ")";
+          });
 
-      // Declare the links…
-      var link = svg.selectAll("path.link")
-        .data(links, function(d) {
-          return d.target.id;
-        });
+        nodeEnter.append("circle")
+          .attr("r", 10)
+          .style("fill", "#fff");
 
-      // Enter the links.
-      link.enter().insert("path", "g")
-        .attr("class", "link")
-        .attr("d", diagonal);
+        nodeEnter.append("text")
+          .attr("y", function(d) {
+            return d.children || d._children ? -18 : 18;
+          })
+          .attr("dy", ".35em")
+          .attr("text-anchor", "middle")
+          .text(function(d) {
+            return d.name;
+          })
+          .style("fill-opacity", 1);
 
-    }
+        // Declare the links…
+        var link = svg.selectAll("path.link")
+          .data(links, function(d) {
+            return d.target.id;
+          });
 
-    $("svg").on('click', '.node', function() {
-      var modal = document.getElementById('modal')
-      modal.showModal();
+        // Enter the links.
+        link.enter().insert("path", "g")
+          .attr("class", "link")
+          .attr("d", diagonal);
 
-      var string = $(this).text();
+      }
 
-      console.log(string)
+      $("svg").on('click', '.node', function() {
+        var modal = document.getElementById('modal')
+        modal.showModal();
 
-      $(document).keydown(function(e) {
-        if (e.keyCode === 27) {
-          e.preventDefault();
-        }
-      })
+        var string = $(this).text();
 
-      $('button').on('click', function() {
-        var name = $('#modal #name').val();
-        var description = $('#modal #description').val();
+        console.log(string)
 
-        modal.close()
-
-        $.ajax({
-          url: '/addSkill/' + string,
-          method: 'post',
-          data: {
-            name: name,
-            description: description
-          },
-          success: function() {
-            modal.style.display = 'none';
+        $(document).keydown(function(e) {
+          if (e.keyCode === 27) {
+            e.preventDefault();
           }
         })
+
+        $('button').on('click', function() {
+          var name = $('#modal #name').val();
+          var description = $('#modal #description').val();
+
+          modal.close()
+
+          $.ajax({
+            url: '/addSkill/' + string,
+            method: 'post',
+            data: {
+              name: name,
+              description: description
+            },
+            success: function() {
+              $('svg').remove();
+              var treeData = [];
+
+              $.ajax({
+                url: '/getSkills',
+                method: 'post',
+                success: function(resp) {
+                  treeData.push(resp);
+                  var margin = {
+                      top: 40,
+                      right: 120,
+                      bottom: 20,
+                      left: 120
+                    },
+                    width = 960 - margin.right - margin.left,
+                    height = 500 - margin.top - margin.bottom;
+
+                  var i = 0;
+
+                  var tree = d3.layout.tree()
+                    .size([height, width]);
+
+                  var diagonal = d3.svg.diagonal()
+                    .projection(function(d) {
+                      return [d.x, d.y];
+                    });
+
+                  var svg = d3.select(".mainTree").append("svg")
+                    .attr("width", width + margin.right + margin.left)
+                    .attr("height", height + margin.top + margin.bottom)
+                    .append("g")
+                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+                  root = treeData[0];
+
+                  update(root);
+
+                  function update(source) {
+
+                    // Compute the new tree layout.
+                    var nodes = tree.nodes(root).reverse(),
+                      links = tree.links(nodes);
+
+                    // Normalize for fixed-depth.
+                    nodes.forEach(function(d) {
+                      d.y = d.depth * 100;
+                    });
+
+                    // Declare the nodes…
+                    var node = svg.selectAll("g.node")
+                      .data(nodes, function(d) {
+                        return d.id || (d.id = ++i);
+                      });
+
+                    // Enter the nodes.
+                    var nodeEnter = node.enter().append("g")
+                      .attr("class", "node")
+                      .attr("transform", function(d) {
+                        return "translate(" + d.x + "," + d.y + ")";
+                      });
+
+                    nodeEnter.append("circle")
+                      .attr("r", 10)
+                      .style("fill", "#fff");
+
+                    nodeEnter.append("text")
+                      .attr("y", function(d) {
+                        return d.children || d._children ? -18 : 18;
+                      })
+                      .attr("dy", ".35em")
+                      .attr("text-anchor", "middle")
+                      .text(function(d) {
+                        return d.name;
+                      })
+                      .style("fill-opacity", 1);
+
+                    // Declare the links…
+                    var link = svg.selectAll("path.link")
+                      .data(links, function(d) {
+                        return d.target.id;
+                      });
+
+                    // Enter the links.
+                    link.enter().insert("path", "g")
+                      .attr("class", "link")
+                      .attr("d", diagonal);
+
+                  }
+
+                  $("svg").on('click', '.node', function() {
+                    var modal = document.getElementById('modal')
+                    modal.showModal();
+
+                    string = $(this).text();
+
+                    console.log(string)
+
+                    $(document).keydown(function(e) {
+                      if (e.keyCode === 27) {
+                        e.preventDefault();
+                      }
+                    })
+
+                    $('button').on('click', function() {
+                      var name = $('#modal #name').val();
+                      var description = $('#modal #description').val();
+
+                      modal.close()
+
+                      $.ajax({
+                        url: '/addSkill/' + string,
+                        method: 'post',
+                        data: {
+                          name: name,
+                          description: description
+                        },
+                        success: function() {
+                          $('svg').remove()
+                        }
+                      })
+                    })
+                  })
+                },
+                error: function(err) {
+                  console.log(err);
+                }
+              });
+            }
+          })
+        })
       })
-    })
-  },
-  error: function(err) {
-    console.log(err);
-  }
-});
+    },
+    error: function(err) {
+      console.log(err);
+    }
+  });
